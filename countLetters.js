2365c137d3679f407dce4c26aefc475759f3afd2
Kenneth <krbrown@live.ca>
2018-01-17 17:29:53 +0000

    removed white spaces

---
+++ countLetters.js
@@ -0,0 +1,11 @@
+//var word = process.argv.slice(2);
+// var someString = "lighthouse labs";
+// var letters = someString.split("")
+// console.log(letters);
+
+function countLetters (word) {
+	var noSpace = word.replace(/\s/g, '');
+ console.log(noSpace);
+ }
+
+ countLetters("light house");
